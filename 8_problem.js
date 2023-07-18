275d1ea4a06911f698ae059fb1a5f8ed423318f0
Vignesh <admin@dreamtusk.com>
2023-07-18 11:29:00 +0530

    8

---
+++ 8_problem.js
@@ -0,0 +1,24 @@
+const prompt = require("prompt-sync")();
+
+const N = prompt("Enter Input NO: ");
+
+let output = ""
+
+for (let i = 1; i <= N; i += 1) {
+
+  if (i % 3== 0 && i % 5== 0) {
+     output += "FizzBuzz "
+
+  } else if (i % 3 == 0) {
+
+     output += "Fizz "
+  } else if (i % 5 == 0) {
+
+     output += "Buzz "
+
+  } else {
+    output += i + " ";
+  }
+}
+
+console.log(output)
