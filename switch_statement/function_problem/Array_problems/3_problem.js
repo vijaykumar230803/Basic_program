7adccd9032b06436a9f22fa860f9ea7cbad4d28e
Vignesh <admin@dreamtusk.com>
2023-07-26 13:51:35 +0530

    3

---
+++ switch_statement/function_problem/Array_problems/3_problem.js
@@ -0,0 +1,18 @@
+const prompt = require("prompt-sync")();
+
+const N = parseInt(prompt("Enter Total No: "));
+
+const Number = [];
+
+for (let i = 0; i < N; i += 1) {
+    const number = parseInt(prompt(`Enter Input No ${i + 1}: `));
+    Number.push(number); 
+}
+
+function sortarray(Array) {
+    Array.sort((a, b) => b - a);
+}
+
+sortarray(Number); 
+
+console.log(Number);
