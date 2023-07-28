90aafc6e9b5e748904bd5c59373118deb0d64c34
Vignesh <admin@dreamtusk.com>
2023-07-28 15:12:32 +0530

    3

---
+++ switch_statement/function_problem/Array_problems/array_problem_2/3_problem.js
@@ -0,0 +1,33 @@
+const prompt = require("prompt-sync")();
+
+const arr = [];
+
+const numElements = parseInt(prompt("Enter the number of elements in the array: "));
+
+for (let i = 0; i < numElements; i += 1) {
+
+  const input = parseInt(prompt(`Enter element ${i + 1}: `));
+  
+  arr.push(input);
+}
+
+console.log("Array:", arr);
+
+const odd = [];
+const even = [];
+
+for (let i = 0; i < arr.length; i+=1) {
+
+  if (arr[i] % 2== 0) {
+
+    even.push(arr[i]);
+  } else {
+
+    odd.push(arr[i]);
+
+  }
+}
+
+console.log("Odd numbers:", odd);
+
+console.log("Even numbers:", even);
